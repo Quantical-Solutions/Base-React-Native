83f64b23da73e7a2b48b9c9753f397c3895820e2
Fred <in-touch-fg@quanticalsolutions.com>
2022-01-13 15:07:58 +0100

    text handler

--- views/Feeds.js
+++ views/Feeds.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, FlatList, Image, View, StyleSheet } from "react-native";
+import { Text, FlatList, Image, View, StyleSheet, TextInput } from "react-native";
 import XHR from "../utils/XHR";
 
 const call = 'https://api.deezer.com/search?q='
@@ -13,6 +13,13 @@ export default class Feeds extends React.Component {
         }
     }
 
+    handleText = (text) => {
+
+        XHR('deezer', call, text, (response) => {
+            this.setState({data: response})
+        })
+    }
+
     componentDidMount() {
 
         XHR('deezer', call, 'Guns n Roses', (response) => {
@@ -22,15 +29,22 @@ export default class Feeds extends React.Component {
 
     render() {
 
-        console.log(this.state.data)
-
         return(
             <View style={styles.container}>
-                <Text>Liste des albums :</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Taper le nom d'un artiste"
+                    onChangeText={text => this.handleText(text)}
+                />
                 <FlatList
                     data={this.state.data}
                     renderItem={( {item} ) =>
-                        <Text style={styles.item}>{item.album.title}</Text>
+                        <View style={styles.album}>
+                            <Text style={styles.title}>{item.album.title}</Text>
+                            <View style={styles.imgContainer}>
+                                <Image source={{uri: item.album.cover}} style={styles.img} />
+                            </View>
+                        </View>
                     }
                 />
             </View>
@@ -41,11 +55,64 @@ export default class Feeds extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 22
+        paddingTop: 20
     },
-    item: {
-        padding: 10,
+    album: {
+        padding: 20,
+        margin: 20,
         fontSize: 18,
-        height: 44
+        marginBottom: 30,
+        backgroundColor: '#E7E7E7',
+        borderRadius: 10,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: '#fff',
+        shadowOffset: { width: 15, height: 15 },
+        shadowColor: 'black',
+        shadowOpacity: 1,
+        elevation: 4,
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        textAlign: 'center',
+        paddingBottom: 20,
+        borderStyle: 'solid',
+        borderBottomWidth: 1,
+        borderBottomColor: '#fff',
+    },
+    input: {
+        margin: 20,
+        padding: 10,
+        borderStyle: 'solid',
+        borderBottomWidth: 1,
+        borderBottomColor: '#E1E1E1',
+        backgroundColor: '#fff',
+        overflow: 'hidden',
+        shadowOffset: { width: 15, height: 15 },
+        shadowColor: 'black',
+        shadowOpacity: 1,
+        elevation: 4,
+        borderRadius: 8
+    },
+    imgContainer: {
+        height: 80,
+        width: 80,
+        marginTop: 20,
+        marginBottom: 10,
+        borderRadius: 10,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: '#A1A1A1',
+        overflow: 'hidden',
+        shadowOffset: { width: 15, height: 15 },
+        shadowColor: 'black',
+        shadowOpacity: 1,
+        elevation: 4,
+        backgroundColor: '#fff'
+    },
+    img: {
+        height: 80,
+        width: 80
     }
 })
